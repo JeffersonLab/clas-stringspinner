ce8e354df07e73ad7cf9d49f2aeaaa8323974dba
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-04-15 18:43:04 -0400

    fix: drop old `draw_mx` script, replace with `draw_mx_2`

--- draw_mx.C
+++ draw_mx.C
@@ -1,19 +1,29 @@
-// ninja && clas-stringspinner --seed 29877 --trig 10000 --cut-inclusive 11,211,-211|grep M_X| awk '{print $2}' > output.dat
-void draw_mx(TString file_name = "output.dat", TString title = "m_x") {
+// run this using `test_mx.sh`
+void draw_mx(TString file_name = "output.both.dat") {
+  gStyle->SetOptStat(0);
   auto tr = new TTree();
-  tr->ReadFile(file_name, "mx/D");
+  tr->ReadFile(file_name, "mx_before/D:mx_after/D");
   // new TCanvas();
   // tr->Draw("mx");
   auto c = new TCanvas("c", "c", 800, 600);
-  auto mx_dist = new TH1D("mx_dist",title,100,0.9,1.5);
-  tr->Project("mx_dist", "mx");
-  mx_dist->Draw();
-  auto peak = mx_dist->GetBinCenter(mx_dist->GetMaximumBin());
-  auto dev = 0.03;
-  // mx_dist->Fit("gaus","","",peak-dev,peak+dev);
-  auto delta = new TLine(1.232, 0, 1.232, mx_dist->GetMaximum());
-  delta->SetLineColor(kRed);
+  auto mx_dist_before = new TH1D("mx_dist_before", "M_{X} distribution", 80, 0.7, 1.35);
+  auto mx_dist_after = new TH1D("mx_dist_after",   "M_{X} distribution", 80, 0.7, 1.35);
+  tr->Project("mx_dist_before", "mx_before");
+  tr->Project("mx_dist_after", "mx_after");
+  mx_dist_before->SetLineColor(kBlue);
+  mx_dist_after->SetLineColor(kRed);
+  mx_dist_after->SetLineStyle(kDashed);
+  mx_dist_after->SetLineWidth(2);
+  mx_dist_before->SetLineWidth(2);
+  mx_dist_before->Draw();
+  mx_dist_after->Draw("same");
+  auto delta = new TLine(1.232, 0, 1.232, mx_dist_before->GetMaximum());
+  delta->SetLineColor(kGreen+1);
   delta->SetLineWidth(3);
   delta->Draw();
+  // auto proton = new TLine(0.938, 0, 0.938, mx_dist_before->GetMaximum());
+  // proton->SetLineColor(kGreen+1);
+  // proton->SetLineWidth(3);
+  // proton->Draw();
   c->SaveAs(file_name+".png");
 }
--- test_mx.sh
+++ test_mx.sh
@@ -10,5 +10,5 @@ build/clas-stringspinner \
   --seed 29877 --trig 10000 --cut-inclusive 11,211,-211 \
   |grep M_X| awk '{print $2}' > output.after_patch.dat
 paste output.before_patch.dat output.after_patch.dat > output.both.dat
-root -b -q draw_mx_2.C'("output.both.dat")'
+root -b -q draw_mx.C'("output.both.dat")'
 nsxiv output.both.dat.png
