aa6be34d3941afe7b8f7e51cb3f7befdaf3625fe
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-08-01 18:16:31 -0400

    feat: control Lund-file float precision (#51)

--- src/EventObjects.h
+++ src/EventObjects.h
@@ -30,36 +30,38 @@ namespace clas {
     /// event weight
     double event_weight;
     /// user values
-    std::vector<double> user_values{};
+    std::vector<int> user_values{};
 
     /// @brief stream to output file
     /// @param output the output file stream
-    void Stream(fmt::ostream& output) const {
+    /// @param precision number of decimal places for floating point numbers
+    void Stream(fmt::ostream& output, int const& precision) const {
       std::size_t const user_values_max_size = 90;
       std::optional<std::string> user_values_str = std::nullopt;
       if(!user_values.empty()) {
         if(user_values.size() <= user_values_max_size) {
-          user_values_str = fmt::format(" {:.5}", fmt::join(user_values, " "));
+          user_values_str = fmt::format(" {:d}", fmt::join(user_values, " "));
         }
         else {
           clas::EventError("LundHeader::user_values is too big, with size = {}; truncating this list", user_values.size());
           decltype(user_values) user_values_trun(user_values_max_size);
           std::copy(user_values.begin(), user_values.begin() + user_values_max_size, user_values_trun.begin());
-          user_values_str = fmt::format(" {:.5}", fmt::join(user_values_trun, " "));
+          user_values_str = fmt::format(" {:d}", fmt::join(user_values_trun, " "));
         }
       }
-      output.print("{:} {:.5} {:} {:} {:} {:} {:.5} {:} {:} {:.5}{}\n",
-          num_particles,
-          target_mass,
-          target_atomic_num,
-          target_spin,
-          beam_spin,
-          beam_type,
-          beam_energy,
-          nucleon_pdg,
-          process_id,
-          event_weight,
-          user_values_str.value_or("")
+      output.print("{num_particles:d} {target_mass:.{p}f} {target_atomic_num:d} {target_spin:.{p}f} {beam_spin:.{p}f} {beam_type:d} {beam_energy:.{p}f} {nucleon_pdg:d} {process_id:d} {event_weight:.{p}f}{user_values}\n",
+          fmt::arg("num_particles", num_particles),
+          fmt::arg("target_mass", target_mass),
+          fmt::arg("target_atomic_num", target_atomic_num),
+          fmt::arg("target_spin", target_spin),
+          fmt::arg("beam_spin", beam_spin),
+          fmt::arg("beam_type", beam_type),
+          fmt::arg("beam_energy", beam_energy),
+          fmt::arg("nucleon_pdg", nucleon_pdg),
+          fmt::arg("process_id", process_id),
+          fmt::arg("event_weight", event_weight),
+          fmt::arg("user_values", user_values_str.value_or("")),
+          fmt::arg("p", precision)
           );
     }
   };
@@ -68,6 +70,7 @@ namespace clas {
 
   /// Lund particle variables
   struct LundParticle {
+
     /// particle index
     int index;
     /// particle lifetime
@@ -99,22 +102,24 @@ namespace clas {
 
     /// @brief stream to output file
     /// @param output the output file stream
-    void Stream(fmt::ostream& output) const {
-      output.print("{:} {:.5} {:} {:} {:} {:} {:.5} {:.5} {:.5} {:.5} {:.5} {:.5} {:.5} {:.5}\n",
-          index,
-          lifetime,
-          type,
-          pdg,
-          mother1,
-          daughter1,
-          px,
-          py,
-          pz,
-          energy,
-          mass,
-          vx,
-          vy,
-          vz
+    /// @param precision number of decimal places for floating point numbers
+    void Stream(fmt::ostream& output, int const& precision) const {
+      output.print("{index:d} {lifetime:.{p}f} {type:d} {pdg:d} {mother1:d} {daughter1:d} {px:.{p}f} {py:.{p}f} {pz:.{p}f} {energy:.{p}f} {mass:.{p}f} {vx:.{p}f} {vy:.{p}f} {vz:.{p}f}\n",
+          fmt::arg("index", index),
+          fmt::arg("lifetime", lifetime),
+          fmt::arg("type", type),
+          fmt::arg("pdg", pdg),
+          fmt::arg("mother1", mother1),
+          fmt::arg("daughter1", daughter1),
+          fmt::arg("px", px),
+          fmt::arg("py", py),
+          fmt::arg("pz", pz),
+          fmt::arg("energy", energy),
+          fmt::arg("mass", mass),
+          fmt::arg("vx", vx),
+          fmt::arg("vy", vy),
+          fmt::arg("vz", vz),
+          fmt::arg("p", precision)
           );
     }
   };
