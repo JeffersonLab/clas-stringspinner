08cf5a836b59a81f65a792b8895d02e0f12eb4cf
Christopher Dilks <c-dilks@users.noreply.github.com>
2025-08-07 17:11:52 -0400

    fix: theta -> cosTheta

--- src/EventObjects.h
+++ src/EventObjects.h
@@ -208,7 +208,7 @@ namespace clas {
     double z{0}; /// dihadron z
     double Mh{0}; /// dihadron invariant mass
     double MX{0}; /// dihadron missing mass
-    double theta{0}; /// dihadron partial wave theta (not scattering angle theta)
+    double cosTheta{0}; /// dihadron partial wave cos(theta) (not scattering angle theta)
 
     static std::function<double(Pythia8::Particle const&, Pythia8::Particle const&)> GetZ(
         Pythia8::Vec4 const& vec_q,
@@ -241,10 +241,10 @@ namespace clas {
       z = GetZ(vec_q, vec_target)(hadA, hadB);
       Mh = vec_Ph.mCalc();
       MX = (vec_target + vec_q - vec_Ph).mCalc();
-      // calculate theta
+      // calculate cos(theta)
       auto vec_hadA__dih = hadA.p();
       vec_hadA__dih.rotbst(Pythia8::toCMframe(vec_Ph));  // boost to dihadron rest frame
-      theta = Pythia8::theta(vec_hadA__dih, vec_Ph);
+      cosTheta = Pythia8::costheta(vec_hadA__dih, vec_Ph);
     }
 
     /// @brief print TTree branch-description
@@ -260,7 +260,7 @@ namespace clas {
             "z/D",
             "Mh/D",
             "MX/D",
-            "theta/D"
+            "cosTheta/D"
             }, ":"));
     }
 
@@ -280,7 +280,7 @@ namespace clas {
               z,
               Mh,
               MX,
-              theta
+              cosTheta
               }, " ")),
           fmt::arg("p", precision)
           );
